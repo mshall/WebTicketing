990fc0591b7daa82ada95bfb3038782a05544a01
MShall <mohamed.s.elshall2011@gmail.com>
2018-08-03 20:56:33 +0400

    Added the terminal serial number

--- WebTicketing/src/main/java/com/shall/customercomplaints/model/Terminal.java
+++ WebTicketing/src/main/java/com/shall/customercomplaints/model/Terminal.java
@@ -25,6 +25,9 @@ public class Terminal {
 	@Column(name = "terminal_id")
 	private Integer terminalId;
 
+	@Column(name = "terminal_serial_number")
+	private String terminalSerialNumber;
+
 	@Column(name = "merchant_id")
 	private int merchantId;
 
